e229c4414081440f0b45d7c04e4fe064b84624ec
byK3 <92488474+byK3@users.noreply.github.com>
2023-05-27 15:35:13 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
-author 'byK3'
-description 'byK3 - Trash Searcher'
+author 'byK3#7147'
+description 'byK3 - Trash Searcher / Hunter'
 
 shared_script '@es_extended/imports.lua'
 
